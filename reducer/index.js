e0fb5c927c402106bb346c4008f7bf1dfa88d92a
brellin <cekakid@gmail.com>
2019-10-07 12:52:56 -0500

    changed something to show greg that we have a webhook now

--- reducer/index.js
+++ reducer/index.js
@@ -14,6 +14,6 @@ export default (state = initialState, action) => {
                 action: action.payload
             }
         default:
-            return state
+            return { ...state }
     }
 }
