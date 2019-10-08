83211f10c64a051d052b4b3e288ee5438e9a7d80
jcrn <clemons.jason@gmail.com>
2019-10-08 12:13:46 -0700

    yarn add: react-native-vector-icons, firebase. ran cmd: react-native link for autoconfig. added initial styling to home.js, created Icon.js component - takes base name from props and adds corresponding prefix as well as passing down the rest of props.

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import firebase from "../firebase"
-import { Button, View, Text, TextInput } from 'react-native'
+import { Button, View, StyleSheet, Text, TextInput } from 'react-native'
+import {Icon} from 'react-native-elements';
 
 function Register(props) {
     const [userName, setUserName] = useState('')
@@ -39,42 +40,62 @@ function Register(props) {
     }
 
     return (
-        <View>
-            <Text>Register for LDSlack</Text>
+        <View style={styles.inputContainer}>
+            <Text style={styles.headerText}>Register for LDSlack</Text>
             <TextInput
                 name="username"
-                placeholder={"Username"}
+                placeholder="Username"
+                style={styles.input}
                 onChangeText={handleChangeUser}
                 value={userName}
                 type="text"
             />
             <TextInput
                 name="email"
-                placeholder={"Email"}
+                placeholder="Email"
+                style={styles.input}
                 onChangeText={handleChangeEmail}
                 value={email}
                 type="text"
             />
             <TextInput
                 name="password"
-                placeholder={"Password"}
+                placeholder="Password"
+                style={styles.input}
                 onChangeText={handleChangePassword}
                 value={password}
                 type="password"
             />
             <TextInput
                 name="passwordConfirm"
-                placeholder={"Confirm Password"}
+                placeholder="Confirm Password"
+                style={styles.input}
                 onChangeText={handleChangePasswordConfirm}
                 value={passwordConfirm}
                 type="password"
             />
-            <Button color="green" title='Sumbit' onPress={handleSubmit} />
+            <Button color="green" title='Submit' onPress={handleSubmit} />
             <Text>Already wearing the undapants?</Text>
         </View>
     )
 }
 
-
+const styles = StyleSheet.create({
+    input: {
+        width: '50%',
+        borderColor: 'black',
+        borderWidth: 1,
+        marginBottom: 10, 
+        padding: 10
+    },
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        padding: 10
+    }
+})
 
 export default Register
