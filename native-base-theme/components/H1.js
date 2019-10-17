aff72a129ca5380aa5f45fb8453e837fc15b9aea
brellin <cekakid@gmail.com>
2019-10-17 17:58:21 -0500

    not fontsworking

--- native-base-theme/components/H1.js
+++ native-base-theme/components/H1.js
@@ -3,7 +3,8 @@
 import variables from '../variables/commonColor'
 
 export default _ => ({
-  color: variables.councils.text.seafoam,
+  color: variables.councils.text.greal,
   fontSize: variables.fontSizeH1,
-  lineHeight: variables.lineHeightH1
+  lineHeight: variables.lineHeightH1,
+  marginVertical: 10
 })
--- native-base-theme/variables/commonColor.js
+++ native-base-theme/variables/commonColor.js
@@ -1,7 +1,7 @@
 // @flow
 
-import color from 'color';
-import { Platform, Dimensions, PixelRatio } from 'react-native';
+import color from 'color'
+import { Platform, Dimensions, PixelRatio } from 'react-native'
 
 const councils = {
   text: {
@@ -171,7 +171,7 @@ export default {
 
   // Font
   DefaultFontSize: 16,
-  fontFamily: platform === PLATFORM.IOS ? 'System' : 'Roboto',
+  fontFamily: platform === PLATFORM.IOS ? 'System' : 'Gotham',
   fontSizeBase: 15,
   get fontSizeH1() {
     return this.fontSizeBase * 3.3;
