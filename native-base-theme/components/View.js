dd7ad1d599eed3af3adb982711f7a2d27dcb02ef
brellin <cekakid@gmail.com>
2019-10-18 15:33:10 -0500

    theming root

--- native-base-theme/components/View.js
+++ native-base-theme/components/View.js
@@ -3,10 +3,31 @@
 import variable from './../variables/platform';
 
 export default (variables /* : * */ = variable) => {
+
+  const { ui, text } = variables.councils
+
   const viewTheme = {
     '.padder': {
       padding: variables.contentPadding
-    }
+    },
+    '.buttonContainerRoot': {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      backgroundColor: ui.eggshell,
+      borderTopWidth: 2,
+      borderTopColor: ui.slightlyLighterEggshell,
+      paddingVertical: 15
+    },
+    '.containerAllRoot': {
+      height: '100%',
+      justifyContent: 'space-between',
+      paddingTop: 25
+    },
+    '.headerContainerRoot': {
+      width: '95%',
+      alignItems: 'center',
+    },
   };
 
   return viewTheme;
